fd56ec04747c01ff9b1112e302a053afd3b9d4f9
PraneethVasa <128016507+PraneethVasa@users.noreply.github.com>
2023-06-11 12:19:48 +0530

    Update copy_of_untitled8.py

--- copy_of_untitled8.py
+++ copy_of_untitled8.py
@@ -1,55 +1,28 @@
 import streamlit as st
+import requests
+from PIL import Image
+import io
+from diffusers import StableDiffusionPipeline
+import torch
 
-import openai
+st.title("Dreamlike Image Generator")
 
-# Set up OpenAI API credentials
-st.caption("Praneeth")
-openai.api_key = "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"
+model_id = "dreamlike-art/dreamlike-photoreal-2.0"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
 
-# Define the chatbot function
+# Create an input text box for the prompt
+prompt = st.text_input("Enter your prompt", "")
 
-def chatbot(message):
+if st.button("Generate Image"):
+    image = pipe(prompt).images[0]
 
-    response = openai.Completion.create(
+    # Display the generated image
+    st.image(image, caption="Generated Image")
 
-        engine="text-davinci-003",
+    # Save the image to a BytesIO object
+    image_io = io.BytesIO()
+    image.save(image_io, format='PNG')
 
-        prompt=message,
-
-        max_tokens=1000,
-
-        temperature=0.6,
-
-        n=1,
-
-        stop=None,
-
-    )
-
-    return response.choices[0].text.strip()
-
-# Set the title and description of the web app
-
-st.title("ChatGPT")
-
-st.write("Welcome to the ChatGPT demo! Enter a message and the model will respond.")
-
-# Get user input
-
-message = st.text_input("User Input")
-
-# Handle user input and generate a response
-
-if st.button("Send"):
-
-    if message:
-
-        with st.spinner("Generating response..."):
-
-            response = chatbot(message)
-
-        st.success(response)
-
-    else:
-
-        st.warning("Please enter a message.")
+    # Send the image as a download link
+    st.download_button("Download Image", data=image_io, file_name='generated_image.png')
